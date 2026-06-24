0910236bd2a10ad648b0cce15a6138554706e508
lindanimasinga <Lindanimasinga@gmail.com>
2026-06-24 19:55:24 +0200

    fix(review): FIX-09 remove PayFast footer link and dead component directories

--- src/app/model/ukheshe-qrode.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UkhesheQrCode } from './ukheshe-qrode';
-
-describe('UkhesheQrode', () => {
-  it('should create an instance', () => {
-    expect(new UkhesheQrCode()).toBeTruthy();
-  });
-});
--- src/app/model/ukheshe-user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UkhesheUser } from './ukheshe-user';
-
-describe('UkhesheUser', () => {
-  it('should create an instance', () => {
-    expect(new UkhesheUser()).toBeTruthy();
-  });
-});
