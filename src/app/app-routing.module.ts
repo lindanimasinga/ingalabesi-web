520b9d33b2464725f1b6793e3ef160e2a9cd3d36
lindanimasinga <Lindanimasinga@gmail.com>
2026-06-24 19:41:27 +0200

    close #12: collapse routing to single-store, remove shortname param and unused routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { ShopItemDescrComponent } from './shop-item-descr/shop-item-descr.component';
 import { HomeComponent } from './home/home.component';
 import { CheckoutComponent } from './checkout/checkout.component';
@@ -8,9 +7,7 @@ import { ShippingComponent } from './shipping/shipping.component';
 import { PaymentComponent } from './payment/payment.component';
 import { OrdersComponent } from './orders/orders.component';
 import { OrderItemHistoryComponent } from './order-item-history/order-item-history.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
 import { MainComponent } from './main/main.component';
-import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
 
 
 const routes: Routes = [
@@ -22,24 +19,8 @@ const routes: Routes = [
       { path: 'cart', component: CheckoutComponent },
       { path: 'shipping', component: ShippingComponent },
       { path: 'payment', component: PaymentComponent },
-      { path: 'store-orders', component: OrdersComponent },
+      { path: 'orders', component: OrdersComponent },
       { path: 'order/:id', component: OrderItemHistoryComponent },
-      { path: 'contact', component: ContactUsComponent },
-      { path: 'terms-conditions', component: TermsConditionsComponent},
-    ]
-  },
-  {
-    path: ':shortname', component: MainComponent,
-    children: [
-      { path: '', component: HomeComponent },
-      { path: 'item/:id', component: ShopItemDescrComponent },
-      { path: 'cart', component: CheckoutComponent },
-      { path: 'shipping', component: ShippingComponent },
-      { path: 'payment', component: PaymentComponent },
-      { path: 'store-orders', component: OrdersComponent },
-      { path: 'order/:id', component: OrderItemHistoryComponent },
-      { path: 'contact', component: ContactUsComponent },
-      { path: 'terms-conditions', component: TermsConditionsComponent},
     ]
   },
 ];
